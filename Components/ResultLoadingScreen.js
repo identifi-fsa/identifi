e50c811704e1df0b41e6546ecd55e508cd5dbdb2
Tyler Swartz <tmswartz12@gmail.com>
2018-11-20 13:58:00 -0500

    updated loading screen to be transparent

--- Components/ResultLoadingScreen.js
+++ Components/ResultLoadingScreen.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react'
-import { Modal, Text } from 'react-native'
+import { Modal, Text, StyleSheet, View } from 'react-native'
 import { Container, Header, Content, Spinner } from 'native-base'
 export default class LoadingScreen extends Component {
   render() {
     console.log('loading', this.props.loading)
     return (
-      <Modal visible={this.props.visibility}>
-        <Text>Comparing Image to Google Cloud</Text>
-        <Spinner color="red" />
-        <Spinner color="green" />
-        <Spinner color="blue" />
+      // <View style={styles.container}>
+      <Modal transparent={true} visible={this.props.visibility}>
+        <View style={styles.container}>
+          <Text>Comparing Image to Google Cloud</Text>
+          <Spinner color="red" />
+          <Spinner color="green" />
+          <Spinner color="blue" />
+        </View>
       </Modal>
+      // </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.3)',
+    width: '100%'
+  },
+  modal: {
+    display: 'flex',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%'
+  }
+})
