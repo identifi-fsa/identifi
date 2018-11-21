f1e9bdb94b92933810bbe197fd3b9587b402a762
jstoobz <stephensja7@gmail.com>
2018-11-21 16:29:47 -0500

    cleaning up files

--- Components/store/places-reducer.js
+++ Components/store/places-reducer.js
@@ -37,7 +37,6 @@ export const fetchNearby = (lat, lng) => async dispatch => {
     const { data } = await axios.get(
       `https://jubjub-server.herokuapp.com/api/places/nearby/${lat}/${lng}`
     )
-    // console.log('THIS IS DATA', dummyData.businesses)
     const action = getNearby(data.businesses)
     dispatch(action)
   } catch (err) {
@@ -51,25 +50,9 @@ const placesReducer = (state = placesState, action) => {
       return { ...state, recent: [action.recent] }
     case GET_NEARBY:
       let hashMap = {}
-      // let narrowedDownData = []
       let results = action.nearby
 
       for (let i = 0; i < results.length; i++) {
-        //the below commented out uses Google NearbyPlaces. We can delete this now as we're using Yelp. Just here incase we need to roll back
-        // let placeObj = {}
-        // placeObj.lat = results[i].geometry.location.lat
-        // placeObj.lng = results[i].geometry.location.lng
-        // placeObj.icon = results[i].icon
-        // placeObj.place_id = results[i].place_id
-        // placeObj.name = results[i].name
-        // placeObj.types = results[i].types
-        // placeObj.vicinity = results[i].vicinity
-        // if (results[i].rating) placeObj.rating = results[i].rating
-        // if (results[i].photos)
-        //   placeObj.photo = results[i].photos[0].photo_reference
-        // if (results[i].photos) placeObj.price_level = results[i].price_level
-        // narrowedDownData.push(placeObj)
-
         const splitNameArr = results[i].name.toLowerCase().split(' ')
         splitNameArr.forEach(word => {
           if (hashMap[word]) {
@@ -79,22 +62,6 @@ const placesReducer = (state = placesState, action) => {
           }
         })
       }
-      // We can delete the commented out lines below. Just here in case we want to remove the hashMap part
-
-      // results.forEach(place => {
-      //   let placeObj = {}
-      //   placeObj.lat = place.geometry.location.lat
-      //   placeObj.lng = place.geometry.location.lng
-      //   placeObj.icon = place.icon
-      //   placeObj.place_id = place.place_id
-      //   placeObj.name = place.name
-      //   placeObj.types = place.types
-      //   placeObj.vicinity = place.vicinity
-      //   if (place.rating) placeObj.rating = place.rating
-      //   if (place.photos) placeObj.photo = place.photos[0].photo_reference
-      //   if (place.photos) placeObj.price_level = place.price_level
-      //   narrowedDownData.push(placeObj)
-      // })
       return { ...state, nearby: results, hashMap: hashMap }
     default:
       return state
