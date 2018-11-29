d05e570a4e83ff9c2ac22e5f718b4d5adda8e913
Tyler Swartz <tmswartz12@gmail.com>
2018-11-29 16:56:09 -0500

    updated colors

--- constants/colors.js
+++ constants/colors.js
@@ -1,48 +1,7 @@
-// import React from 'react'
-// import { connect } from 'react-redux'
-// import { View } from 'react-native'
-// console.log('colorrrrrs', this.state)
-// // export const colors = {
-// //   primary: this.state.primary,
-// //   backgroundColor: this.state.backgroundColor,
-// //   text: this.state.text,
-// //   border: this.state.border,
-// //   disabledNavButton: this.state.disabledNavButton
-// // }
-
-// class Colors extends React.Component {
-//   constructor() {
-//     super()
-//     this.state = {
-//       primary: 'red',
-//       backgroundColor: 'white',
-//       text: 'black',
-//       border: 'black',
-//       disabledNavButton: 'gray'
-//     }
-//   }
-//   export colors = {
-//     primary: this.state.primary,
-//     backgroundColor: this.state.backgroundColor,
-//     text: this.state.text,
-//     border: this.state.border,
-//     disabledNavButton: this.state.disabledNavButton
-//   }
-//   render() {
-//     return <View />
-//   }
-// }
 export const colors = {
   primary: 'red',
-<<<<<<< HEAD
   backgroundColor: 'white',
   text: 'black',
   border: 'black',
   disabledNavButton: 'gray'
-=======
-  border: '#c8c7cc',
-  disabledNavButton: 'grey',
-  backgroundColor: 'black',
-  text: 'white'
->>>>>>> master
 }
